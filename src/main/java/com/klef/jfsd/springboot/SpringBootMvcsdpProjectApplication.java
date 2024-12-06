fe2f0d67eaabcb1905673385b1d2dd61542dced1
Uppu Syam Prasad <syamuppu05@gmail.com>
2024-12-06 16:53:00 +0530

    updated

--- src/main/java/com/klef/jfsd/springboot/SpringBootMvcsdpProjectApplication.java
+++ src/main/java/com/klef/jfsd/springboot/SpringBootMvcsdpProjectApplication.java
@@ -8,7 +8,7 @@ public class SpringBootMvcsdpProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcsdpProjectApplication.class, args);
-		System.out.println("Project is Running...");
+		System.out.println("MVC SDP Project is Running...");
 	}
 
 }
